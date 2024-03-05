6b6a1686eda407f5af4fae730f4302e2e150dced
Abhirup <abhirupkumar001@gmail.com>
2024-03-05 12:11:25 +0530

    Some Bugs Fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,3 +157,21 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+document.onkeydown = (e) => {
+  if (e.key === 123) {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key === 'I') {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key === 'C') {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key === 'J') {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.key === 'U') {
+    e.preventDefault();
+  }
+};
